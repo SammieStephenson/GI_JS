c2ddcca02aa2786ec14e24d6d08da325353637a8
Sammie Stephenson <sammiestephenson@MacBook-Pro.attlocal.net>
2022-04-13 11:34:16 -0400

    Easy

--- scriptJs.js
+++ scriptJs.js
@@ -1,4 +1,16 @@
 let x = 6;
 let y = 2;
-var console;
-console.log(x - y === 4);
+// var console;
+// console.log(x - y === 4);
+
+const firstName = "Sammie"
+const secondName = "Tristyn"
+console.log(firstName, secondName);
+
+
+const firstNameLength = 6
+const secondNameLength = 7
+console.log(secondNameLength > firstNameLength)
+console.log("The name " + firstName + " is shorter than the name  " + secondName + " by 1 letter. ");
+
+
