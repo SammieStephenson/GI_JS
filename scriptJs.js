3470278ce1b8554eb8191aee4963c4c2ec62102c
Sammie Stephenson <sammiestephenson@MacBook-Pro.attlocal.net>
2022-04-13 21:17:53 -0400

    Medium

--- scriptJs.js
+++ scriptJs.js
@@ -13,4 +13,18 @@ const secondNameLength = 7
 console.log(secondNameLength > firstNameLength)
 console.log("The name " + firstName + " is shorter than the name  " + secondName + " by 1 letter. ");
 
+const difference = 1
+if (firstNameLength >= secondNameLength) {
+    console.log("The name " + firstName + " is longer than " + secondName + " by " + difference + ".")
+} else {
+    console.log("The name " + secondName + " is longer than " + firstName + " by " + difference + ".")
+}
 
+let toneOfText = prompt("Hey! What's up? ");
+if (toneOfText === toneOfText.toUpperCase()) {
+    console.log('WHY ARE WE SHOUTING?!')
+} else if (toneOfText === toneOfText.toLowerCase()) {
+    console.log('what are you whispering for?')
+} else {
+    console.log("Now we're talking!")
+}
