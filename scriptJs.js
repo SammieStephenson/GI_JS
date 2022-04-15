6ecbcf6acaf16a4ff4d4353f646152c2e7b6b08b
Sammie Stephenson <sammiestephenson@MacBook-Pro.attlocal.net>
2022-04-15 12:48:44 -0400

    Hard

--- scriptJs.js
+++ scriptJs.js
@@ -28,3 +28,24 @@ if (toneOfText === toneOfText.toUpperCase()) {
 } else {
     console.log("Now we're talking!")
 }
+let num1 = 7
+let num2 = 8
+const add = function (num1, num2) {
+    return num1 + num2
+}
+console.log(add(num1, num2))
+
+const subtract = function (num1, num2) {
+    return num1 - num2
+}
+console.log(subtract(num1, num2))
+
+const multiply = function (num1, num2) {
+    return num1 * num2
+}
+console.log(multiply(num1, num2))
+
+const divide = function (num1, num2) {
+    return num1 / num2
+}
+console.log(divide(num1, num2))
